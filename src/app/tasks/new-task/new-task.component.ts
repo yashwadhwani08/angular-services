8240abb6fc6b643e6435e2a6b53b7f205cc03a5f
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-30 15:24:20 +0530

    using custom Dependency injection token and providers

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TasksService } from '../tasks.service';
+import { TasksServiceToken } from '../../../main';
 
 @Component({
   selector: 'app-new-task',
@@ -12,7 +13,7 @@ import { TasksService } from '../tasks.service';
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
-  private tasksService = inject(TasksService);
+  private tasksService = inject(TasksServiceToken);
 
   onAddTask(title: string, description: string) {
     this.tasksService.addTask({
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-// import { TasksService } from './app/tasks/tasks.service';
+import { TasksService } from './app/tasks/tasks.service';
+import { InjectionToken } from '@angular/core';
 
-// bootstrapApplication(AppComponent, {providers: [TasksService]}).catch((err) => console.error(err));
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+export const TasksServiceToken = new InjectionToken<TasksService>('tasks-service-token'); 
+
+bootstrapApplication(AppComponent, {providers: [{provide: TasksServiceToken, useClass: TasksService}]}).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
